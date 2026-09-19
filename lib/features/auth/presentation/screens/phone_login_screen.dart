f7301bcaad053d8a5cf7b60163bc86e3bf578c25
jobin george <jobingeorge114141@gmail.com>
2026-09-19 19:09:23 +0530

    updated to reomvede

--- lib/features/auth/presentation/screens/phone_login_screen.dart
+++ lib/features/auth/presentation/screens/phone_login_screen.dart
@@ -58,7 +58,7 @@ class LoginScreen extends HookConsumerWidget {
 
                 const SizedBox(height: 40),
                 const Text(
-                  'Login or Sign ',
+                  'Login or Sign remove',
                   style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                 ),
                 const SizedBox(height: 8),
