8071dd63ecb521cb884fb198b1c7f250d4513845
anna <annatheresajoseph@gmail.com>
2026-09-22 20:20:29 +0530

    updated code by anna anna

--- lib/features/auth/presentation/screens/phone_login_screen.dart
+++ lib/features/auth/presentation/screens/phone_login_screen.dart
@@ -58,7 +58,7 @@ class LoginScreen extends HookConsumerWidget {
 
                 const SizedBox(height: 40),
                 const Text(
-                  'Login or Sign sample ',
+                  'Login or Sign anna ',
                   style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                 ),
                 const SizedBox(height: 8),
